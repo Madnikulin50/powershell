0872448c7b4bdced50c998db72f5aebb2453dc72
Maxim Nikulin <Madnikulin50@gmail.com>
2019-07-03 15:40:33 +0300

    Update explore-folder.ps1

--- explore-folder.ps1
+++ explore-folder.ps1
@@ -10,13 +10,21 @@ param (
 $LogDate = get-date -f yyyyMMddhhmm 
 $outfile = "$($outfilename)_$LogDate.json"
 
-Write-Host "base: " $base
-Write-Host "server: " $server
+Write-Host "base: " $folder
 Write-Host "outfile: " $outfile
 
 if (Test-Path $outfile) 
 {
   Remove-Item $outfile
+}
+
+Function Get-FileHashTSO([String] $FileName,$HashName = "SHA1") 
+{
+  $FileStream = New-Object System.IO.FileStream($FileName,"Open", "Read") 
+  $StringBuilder = New-Object System.Text.StringBuilder 
+  [System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash($FileStream)|%{[Void]$StringBuilder.Append($_.ToString("x2"))} 
+  $FileStream.Close() 
+  $StringBuilder.ToString() 
 }
 
 #powershell.exe -ExecutionPolicy Bypass -Command "./explore_share.ps1" -outfilename explore_folder
@@ -26,8 +34,25 @@ Foreach-Object {
     $cur = $_ | Select-Object -Property "Name", "FullName", "BaseName", "CreationTime", "LastAccessTime", "LastWriteTime", "Attributes", "PSIsContainer", "Extension", "Mode"
     Write-Host $_.FullName
     $acl = Get-Acl $_.FullName | Select-Object -Property "Owner", "Group", "AccessToString", "Sddl"
-    $hash = Get-FileHash $_.FullName | Select-Object -Property "Algorithm", "Hash"
+    $path = $_.FullName
+    
+    
+    Try
+    {
+        $hash = Get-FileHashTSO $path
+    }
+    Catch {
+        Write-Host $PSItem.Exception.Message
+        Try
+        {
+            $hash = Get-FileHash $path | Select-Object -Property "Hash"
+        }
+        Catch {
+            Write-Host $PSItem.Exception.Message
+        }
+    }
+    
     $cur | Add-Member -MemberType NoteProperty -Name ACL -Value $acl -Force
     $cur | Add-Member -MemberType NoteProperty -Name Hash -Value $hash -Force
-    $cur
-} | ConvertTo-Json | Out-File -FilePath $outfile -Encoding UTF8 -Append
+    $cur | ConvertTo-Json | Out-File -FilePath $outfile -Encoding UTF8 -Append    
+} 
