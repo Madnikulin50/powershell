41ca449ba72544f2d7165c7f517347e4995f1815
Maxim Nikulin <Madnikulin50@gmail.com>
2019-07-04 18:08:30 +0300

    Update export-ad.ps1

--- export-ad.ps1
+++ export-ad.ps1
@@ -15,7 +15,7 @@ $LogDate = get-date -f yyyyMMddhhmm
 Import-Module ActiveDirectory
 
 $SearchBase = $base 
-$outfile = "$($outfilename).json"
+$outfile = "$($outfilename)_$LogDate.json"
 
 if (Test-Path $outfile) 
 {
@@ -26,44 +26,41 @@ if (Test-Path $outfile)
 $GetAdminact = Get-Credential
 
 
-$AllADGroups = Get-ADGroup -server $server `
+Get-ADGroup -server $server `
 -Credential $GetAdminact -searchbase $SearchBase `
--Filter * -Properties * | Where-Object {$_.info -NE 'Migrated'}
-
-$groups = $AllADGroups | Select-Object "Name", "GivenName", "Surname", "dn", "sn", "cn", "distinguishedName",
+-Filter * -Properties * | Where-Object {$_.info -NE 'Migrated'} | Select-Object "Name", "GivenName", "Surname", "sn", "cn", "distinguishedName",
 "whenCreated", "whenChanged", "memberOf", "objectSid", "DisplayName", 
 "sAMAccountName", "StreetAddress", "City", "state", "PostalCode", "Country", "Title",
 "Company", "Description", "Department", "OfficeName", "telephoneNumber", "thumbnailPhoto",
-"Mail", "userAccountControl", "PasswordNeverExpires", "PasswordExpired", "DoesNotRequirePreAuth",
-"CannotChangePassword", "PasswordNotRequired", "TrustedForDelegation", "TrustedToAuthForDelegation",
-"Manager", "Enabled", "ObjectClass"
-
-$groups | ConvertTo-Json | Out-File -FilePath $outfile -Encoding UTF8 -Append
+"Mail", "userAccountControl", "Manager", "ObjectClass" | Foreach-Object {
+  $cur = $_  
+  $cur | ConvertTo-Json | Out-File -FilePath $outfile -Encoding UTF8 -Append
+}
 
 Write-Host "groups export finished to: " $outfile
 
-$AllADUsers = Get-ADUser -server $server `
+Get-ADUser -server $server `
 -Credential $GetAdminact -searchbase $SearchBase `
--Filter * -Properties * | Where-Object {$_.info -NE 'Migrated'}
-
-$users = $AllADUsers | Select-Object "Name", "GivenName", "Surname", "dn", "sn", "cn", "distinguishedName",
+-Filter * -Properties * | Where-Object {$_.info -NE 'Migrated'} | Select-Object "Name", "GivenName", "Surname", "sn", "cn", "distinguishedName",
 "whenCreated", "whenChanged", "memberOf", "badPwdCount", "objectSid", "DisplayName", 
 "sAMAccountName", "StreetAddress", "City", "state", "PostalCode", "Country", "Title",
 "Company", "Description", "Department", "OfficeName", "telephoneNumber", "thumbnailPhoto",
 "Mail", "userAccountControl", "PasswordNeverExpires", "PasswordExpired", "DoesNotRequirePreAuth",
 "CannotChangePassword", "PasswordNotRequired", "TrustedForDelegation", "TrustedToAuthForDelegation",
-"Manager", "Enabled", "lastlogondate", "ObjectClass"
-
-$users | ConvertTo-Json | Out-File -FilePath $outfile -Encoding UTF8 -Append
+"Manager", "Enabled", "lastlogondate", "ObjectClass" | Foreach-Object {
+  $cur = $_  
+  $cur | ConvertTo-Json | Out-File -FilePath $outfile -Encoding UTF8 -Append
+}
 
 Write-Host "users export finished to: " $outfile
 
-$computers = Get-ADComputer -Filter * -server $server `
+Get-ADComputer -Filter * -server $server `
 -Credential $GetAdminact -searchbase $SearchBase ` |
 Select-Object "Name", "dn", "sn", "cn", "distinguishedName", "whenCreated", "whenChanged", "memberOf", "badPwdCount", "objectSid", "DisplayName", 
 "sAMAccountName", "IPv4Address", "IPv6Address", "OperatingSystem", "OperatingSystemHotfix", "OperatingSystemServicePack", "OperatingSystemVersion",
-"PrimaryGroup", "ManagedBy", "userAccountControl", "Enabled", "lastlogondate", "ObjectClass"
-
-$computers | ConvertTo-Json | Out-File -FilePath $outfile -Encoding UTF8 -Append
+"PrimaryGroup", "ManagedBy", "userAccountControl", "Enabled", "lastlogondate", "ObjectClass" | Foreach-Object {
+  $cur = $_  
+  $cur | ConvertTo-Json | Out-File -FilePath $outfile -Encoding UTF8 -Append
+}
 
 Write-Host "computers export finished to: " $outfile
